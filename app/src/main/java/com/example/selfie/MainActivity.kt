243f45baef18069f01845782dfdfb6c5054d26de
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2022-04-29 12:22:37 +0300

    Change MainActivity.kt

--- app/src/main/java/com/example/selfie/MainActivity.kt
+++ app/src/main/java/com/example/selfie/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.selfie
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.example.selfie.databinding.ActivityMainBinding
 import java.io.File
@@ -50,43 +48,28 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Take a photo first!", Toast.LENGTH_SHORT).show()
             }
             else {
-                val intent = Intent(Intent.ACTION_SEND)
-
-                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("hodovychenko.labs@gmail.com"))
-                intent.putExtra(Intent.EXTRA_SUBJECT, "КПП АИ-192 Таужняский А.В.")
-                intent.putExtra(Intent.EXTRA_TEXT, "Sending nudes")
-                intent.putExtra(Intent.EXTRA_STREAM, photoURI)
-                intent.type = "message/rfc822"
-
-                try {
-                    startActivity(Intent.createChooser(intent, "Select an application"))
-                }
-                catch (ex: ActivityNotFoundException) {
-                    Toast.makeText(this, "No supported application", Toast.LENGTH_SHORT).show()
-                }
+                sendMailIntent()
             }
         }
     }
 
     @Throws(IOException::class)
     private fun getImageFile(): File {
-        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageName = "jpg_$timestamp"
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-
-        return File.createTempFile(imageName, ".jpg", storageDir)
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.UK).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = applicationContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",  /* suffix */
+            storageDir /* directory */
+        )
     }
 
     private fun captureImage() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
         if (intent.resolveActivity(packageManager) != null) {
-            val imageFile: File? = try {
-                getImageFile()
-            } catch (ex: IOException) {
-                Toast.makeText(this, "Error occurred while creating file!", Toast.LENGTH_SHORT).show()
-                null
-            }
+            val imageFile: File? = getImageFile()
 
             if (imageFile != null) {
                 val imageURI = FileProvider.getUriForFile(
@@ -100,5 +83,18 @@ class MainActivity : AppCompatActivity() {
         }
         else Toast.makeText(this, "No supported application", Toast.LENGTH_SHORT).show()
 
+    }
+
+
+    private fun sendMailIntent() {
+        val intent = Intent(Intent.ACTION_SEND)
+
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("hodovychenko.labs@gmail.com"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "КПП АИ-192 Таужняский А.В.")
+        intent.putExtra(Intent.EXTRA_TEXT, "Sending nudes")
+        intent.putExtra(Intent.EXTRA_STREAM, photoURI)
+        intent.type = "message/rfc822"
+
+        startActivity(Intent.createChooser(intent, "Select an application"))
     }
 }
