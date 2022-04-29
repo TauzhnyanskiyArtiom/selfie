d4993a9519688c740988787164b6b3b0ae31a112
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2022-04-29 12:02:22 +0300

    Change MainActivity.kt

--- app/src/main/java/com/example/selfie/MainActivity.kt
+++ app/src/main/java/com/example/selfie/MainActivity.kt
@@ -1,109 +1,102 @@
 package com.example.selfie
 
-import android.Manifest
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
-import com.example.selfie.databinding.ActivityMainBinding
-import java.io.*
+import com.example.lab3.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private var latestUri: Uri? = null
-
-    private val getCameraImage =
-        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
-            if (success) {
-                latestUri?.let { uri ->
-                    binding.selfie.setImageURI(uri)
-                }
-            } else {
-                Log.i("INFO", "Picture captured with errors")
-            }
-        }
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            Toast.makeText(
-                baseContext,
-                "We cannot do photos without your permission!",
-                Toast.LENGTH_SHORT
-            ).show()
-        } else {
-            Toast.makeText(
-                baseContext,
-                "We cannot do photos without your permission!",
-                Toast.LENGTH_LONG
-            ).show()
-        }
-    }
+    private lateinit var photoURI: Uri
+    private lateinit var cameraLauncher: ActivityResultLauncher<Uri>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnTake.setOnClickListener {
-            grantCameraPermission()
-            latestUri = getFileUri()
-            getCameraImage.launch(latestUri)
+        val photoBox = binding.photo
+
+        // Using Activity Result API to start camera activity and handle result
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            when (it) {
+                true -> photoBox.setImageURI(photoURI)
+                false -> Toast.makeText(this, "Operation was canceled!", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        binding.btnPhoto.setOnClickListener {
+            captureImage()
+        }
+
         binding.btnSend.setOnClickListener {
-            sendMailIntent()
-        }
-    }
-
-    private fun grantCameraPermission() {
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(
-                baseContext,
-                Manifest.permission.CAMERA
-            ) -> {
-                Log.i("TAG", "Camera permissions granted already")
+            if (photoBox.drawable == null) {
+                Toast.makeText(this, "Take a photo first!", Toast.LENGTH_SHORT).show()
             }
-            else -> {
-                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            else {
+                val intent = Intent(Intent.ACTION_SEND)
+
+                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("hodovychenko.labs@gmail.com"))
+                intent.putExtra(Intent.EXTRA_SUBJECT, "КПП АИ-192 Таужняский А.В.")
+                intent.putExtra(Intent.EXTRA_TEXT, "Sending nudes")
+                intent.putExtra(Intent.EXTRA_STREAM, photoURI)
+                intent.type = "message/rfc822"
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Select an application"))
+                }
+                catch (ex: ActivityNotFoundException) {
+                    Toast.makeText(this, "No supported application", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
-    private fun getFileUri(): Uri {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.UK).format(Date())
-        val imageFileName = "JPEG_" + timeStamp + "_"
-        val storageDir = applicationContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            imageFileName,  /* prefix */
-            ".jpg",  /* suffix */
-            storageDir /* directory */
-        )
-        return FileProvider.getUriForFile(this, "com.example.selfie.fileprovider", image)
+    @Throws(IOException::class)
+    private fun getImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageName = "jpg_$timestamp"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return File.createTempFile(imageName, ".jpg", storageDir)
     }
 
-    private fun sendMailIntent() {
-        latestUri?.let {
-            val emailIntent = Intent(Intent.ACTION_SEND)
-            emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            emailIntent.setDataAndType(it, contentResolver.getType(it))
-            emailIntent.putExtra(Intent.EXTRA_STREAM, it)
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("hodovychenko.labs@gmail.com"))
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "КПП Таужнянский АИ-192")
-            startActivity(Intent.createChooser(emailIntent, "Send with..."))
+    private fun captureImage() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            val imageFile: File? = try {
+                getImageFile()
+            } catch (ex: IOException) {
+                Toast.makeText(this, "Error occurred while creating file!", Toast.LENGTH_SHORT).show()
+                null
+            }
+
+            if (imageFile != null) {
+                val imageURI = FileProvider.getUriForFile(
+                    this,
+                    "com.example.android.fileprovider",
+                    imageFile)
+                photoURI = imageURI
+
+                cameraLauncher.launch(imageURI)
+            }
         }
+        else Toast.makeText(this, "No supported application", Toast.LENGTH_SHORT).show()
+
     }
 }
